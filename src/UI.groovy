e24603999c18e03852703098429cbc5ff4840929
sonerik <thesonerik@gmail.com>
2015-03-18 10:07:54 +0200

    More work on UI

--- src/UI.groovy
+++ src/UI.groovy
@@ -12,14 +12,24 @@ import java.awt.event.ActionEvent
 class UI {
 
     class InputsModel {
-        @Bindable String learningRate = "0.115"
-        @Bindable String maxEpochs = "10000"
-        @Bindable String hiddenLayers = "2"
+        @Bindable
+        String learningRate = "0.115"
+        @Bindable
+        String maxEpochs = "10000"
+        @Bindable
+        String hiddenLayers = "2"
     }
 
     def inputsModel = new InputsModel()
 
     JPanel chartPanelContainer
+    JTable inputTable
+    JTable outputTable
+
+    def tableHeaders = ["x1", "x2", "x3", "x4", "x5", "x6", "y"]
+    def tableData = [
+            ["x1", "x2", "x3", "x4", "x5", "x6", "y"] * 5
+    ]
 
     def chart
 
@@ -27,7 +37,7 @@ class UI {
         chartPanelContainer?.removeAll();
         chartPanelContainer?.revalidate(); // This removes the old chart
         ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanelContainer?.setPreferredSize(new Dimension(1024, 720));
+        chartPanelContainer?.setPreferredSize(new Dimension(1024, 480));
         chartPanelContainer?.add(chartPanel);
         chartPanelContainer?.repaint(); // This method makes the new chart appear
     }
@@ -51,36 +61,36 @@ class UI {
                 title: 'Neural Network',
                 pack: true,
                 show: true,
-                preferredSize: new Dimension(1024, 768),
+                preferredSize: new Dimension(1280, 768),
                 defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
 
             panel {
                 tableLayout {
                     tr {
-                        td {
-                            panel(id: 'controls', preferredSize: new Dimension(1024, 48)) {
+                        td(colspan: 2, rowfill: 2) {
+                            panel(id: 'controls', preferredSize: new Dimension(1280, 48)) {
 
-                                label(text:"Learning rate:")
+                                label(text: "Learning rate:")
                                 textField(
-                                        preferredSize: new Dimension(200, 24),
+                                        preferredSize: new Dimension(60, 24),
                                         text: bind('learningRate', source: inputsModel, mutual: true),
                                         action: action(name: 'changeLearningRate', closure: {
                                             ActionEvent e -> println "Learning rate: ${e.actionCommand}"
                                         })
                                 )
 
-                                label(text:"Max epochs:")
+                                label(text: "Max epochs:")
                                 textField(
-                                        preferredSize: new Dimension(200, 24),
+                                        preferredSize: new Dimension(60, 24),
                                         text: bind('maxEpochs', source: inputsModel, mutual: true),
                                         action: action(name: 'changeLearningRate', closure: {
                                             ActionEvent e -> println "Learning rate: ${e.actionCommand}"
                                         })
                                 )
 
-                                label(text:"Hidden layers:")
+                                label(text: "Hidden layers:")
                                 textField(
-                                        preferredSize: new Dimension(200, 24),
+                                        preferredSize: new Dimension(60, 24),
                                         text: bind('hiddenLayers', source: inputsModel, mutual: true),
                                         action: action(name: 'changeLearningRate', closure: {
                                             ActionEvent e -> println "Learning rate: ${e.actionCommand}"
@@ -88,7 +98,7 @@ class UI {
                                 )
 
                                 button(preferredSize: new Dimension(64, 24), action: action(name: 'Run', closure: {
-                                    def statement = new File('input.txt').readLines().find {!it.startsWith("#")}
+                                    def statement = new File('input.txt').readLines().find { !it.startsWith("#") }
                                     def patterns = new StatementParser().generateTable(statement)
 
                                     Perceptron perceptron = new Perceptron(patterns,
@@ -113,8 +123,47 @@ class UI {
 
                     tr {
                         td {
-                            chartPanelContainer = panel(id: 'canvas', preferredSize: new Dimension(1024, 720)) {
+                            chartPanelContainer = panel(id: 'canvas', preferredSize: new Dimension(680, 480)) {
                                 widget(new ChartPanel(chart))
+                            }
+                        }
+                        td {
+                            panel(id: 'canvas', preferredSize: new Dimension(500, 480)) {
+
+                                boxLayout(axis: BoxLayout.Y_AXIS)
+                                label text: "Input values"
+                                scrollPane() {
+                                    table() {
+                                        def model = Collections.nCopies(500, 1..7)
+
+                                        tableModel(list: model) {
+                                            closureColumn(header: 'x1', read: { row -> return row[0] })
+                                            closureColumn(header: 'x2', read: { row -> return row[1] })
+                                            closureColumn(header: 'x3', read: { row -> return row[2] })
+                                            closureColumn(header: 'x4', read: { row -> return row[3] })
+                                            closureColumn(header: 'x5', read: { row -> return row[4] })
+                                            closureColumn(header: 'x6', read: { row -> return row[5] })
+                                            closureColumn(header: 'y', read: { row -> return row[6] })
+                                        }
+                                    }
+                                }
+
+                                label text: "Output values"
+                                scrollPane() {
+                                    table() {
+                                        def model = Collections.nCopies(5, 1..7)
+
+                                        tableModel(list: model) {
+                                            closureColumn(header: 'x1', read: { row -> return row[0] })
+                                            closureColumn(header: 'x2', read: { row -> return row[1] })
+                                            closureColumn(header: 'x3', read: { row -> return row[2] })
+                                            closureColumn(header: 'x4', read: { row -> return row[3] })
+                                            closureColumn(header: 'x5', read: { row -> return row[4] })
+                                            closureColumn(header: 'x6', read: { row -> return row[5] })
+                                            closureColumn(header: 'y', read: { row -> return row[6] })
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
